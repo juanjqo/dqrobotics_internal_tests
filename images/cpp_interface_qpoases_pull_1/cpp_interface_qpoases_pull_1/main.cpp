8d7c6e969027549186c64e4d78f3a795af7d10fa
root <juanjqogm@gmail.com>
2025-08-04 13:49:44 +0000

    updated the test example

--- images/cpp_interface_qpoases_pull_1/cpp_interface_qpoases_pull_1/main.cpp
+++ images/cpp_interface_qpoases_pull_1/cpp_interface_qpoases_pull_1/main.cpp
@@ -16,11 +16,11 @@ int main()
     double sparsity_factor{0.15};
     double eps_abs{1e-9};
     proxqp::utils::rand::set_seed(1);
-    for (proxqp::isize dim = 10; dim < 300; dim += 10)
+    for (proxqp::isize dim = 10; dim < 100; dim += 10)
     {
         std::cout<<"---------------"<<std::endl;
         std::cout<<"Dimension: "<<dim<<std::endl;
-        proxqp::isize n_eq(dim);
+        proxqp::isize n_eq(10);
         proxqp::isize n_in(dim);
         double strong_convexity_factor{1.e-2};
         proxqp::dense::Model<double> qp_random = proxqp::utils::dense_box_constrained_qp(
@@ -40,44 +40,13 @@ int main()
         std::cout<<"u_proxqp : "<<(u_proxqp).transpose()<<std::endl;
 
         DQ_robotics::DQ_QPOASESSolver qpoases_solver;
+        qpoases_solver.set_equality_constraints_tolerance(eps_abs);
 
         auto u_qpoases = qpoases_solver.solve_quadratic_program(H, f, A, b, Aeq, beq);
         std::cout<<"u_qpoases: "<<(u_qpoases).transpose()<<std::endl;
 
         std::cout<<"error: "<<(u_proxqp-u_qpoases).norm()<<std::endl;
         std::cout<<"---------------"<<std::endl;
-
-
-
-        /*
-        proxqp::dense::QP<double> qp{ dim, n_eq, n_in }; // creating QP object
-        qp.settings.eps_abs = eps_abs;
-        qp.settings.eps_rel = 0;
-
-        qp.init(qp_random.H,
-                qp_random.g,
-                qp_random.A,
-                qp_random.b,
-                qp_random.C,
-                -1*Eigen::VectorXd::Ones(dim)*INFINITY,
-                qp_random.u);
-        qp.solve();
-        double pri_res = std::max(
-            (qp_random.A * qp.results.x - qp_random.b).lpNorm<Eigen::Infinity>(),
-            (helpers::positive_part(qp_random.C * qp.results.x - qp_random.u) +
-             helpers::negative_part(qp_random.C * qp.results.x - qp_random.l))
-                .lpNorm<Eigen::Infinity>());
-        double dua_res = (qp_random.H * qp.results.x + qp_random.g +
-                     qp_random.A.transpose() * qp.results.y +
-                     qp_random.C.transpose() * qp.results.z)
-                        .lpNorm<Eigen::Infinity>();
-        std::cout << "------solving qp with dim: " << dim << " neq: " << n_eq
-                  << " nin: " << n_in << std::endl;
-        std::cout << "primal residual: " << pri_res << std::endl;
-        std::cout << "dual residual: " << dua_res << std::endl;
-        std::cout << "total number of iteration: " << qp.results.info.iter
-                  << std::endl;
-*/
     }
     return 0;
 }
