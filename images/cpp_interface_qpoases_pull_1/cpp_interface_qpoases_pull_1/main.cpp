dc68fa9da802d1f646f1dd9604febccf222749e3
juanjqo <juanjqogm@gmail.com>
2025-08-04 14:42:12 +0000

    updated the example test

--- images/cpp_interface_qpoases_pull_1/cpp_interface_qpoases_pull_1/main.cpp
+++ images/cpp_interface_qpoases_pull_1/cpp_interface_qpoases_pull_1/main.cpp
@@ -7,7 +7,7 @@
 #include <dqrobotics/solvers/DQ_PROXQPSolver.h>
 #include <dqrobotics/solvers/DQ_QPOASESSolver.h>
 
-using namespace std;
+
 using namespace proxsuite;
 
 int main()
@@ -16,11 +16,14 @@ int main()
     double sparsity_factor{0.15};
     double eps_abs{1e-9};
     proxqp::utils::rand::set_seed(1);
-    for (proxqp::isize dim = 10; dim < 100; dim += 10)
+
+    std::vector<double> error_list;
+    int i=0;
+    for (proxqp::isize dim = 10; dim <= 200; dim += 10)
     {
         std::cout<<"---------------"<<std::endl;
         std::cout<<"Dimension: "<<dim<<std::endl;
-        proxqp::isize n_eq(10);
+        proxqp::isize n_eq(dim/2);
         proxqp::isize n_in(dim);
         double strong_convexity_factor{1.e-2};
         proxqp::dense::Model<double> qp_random = proxqp::utils::dense_box_constrained_qp(
@@ -32,9 +35,12 @@ int main()
         VectorXd beq = qp_random.b;
         MatrixXd A = qp_random.C;
         VectorXd b = qp_random.u;
+        VectorXd l = -1*VectorXd::Ones(b.size())*INFINITY;
 
-        DQ_robotics::DQ_PROXQPSolver proxqp_solver;
-        auto u_proxqp = proxqp_solver.solve_quadratic_program(H, f, A, b, Aeq, beq);
+        proxsuite::proxqp::dense::QP<double> qp(dim, n_eq, n_in);
+        qp.init(H,f,Aeq,beq,A,l,b); // initialize the model
+        qp.solve();
+        auto u_proxqp = qp.results.x;
         std::cout<<"u_proxqp : "<<(u_proxqp).transpose()<<std::endl;
 
         DQ_robotics::DQ_QPOASESSolver qpoases_solver;
@@ -43,8 +49,15 @@ int main()
         auto u_qpoases = qpoases_solver.solve_quadratic_program(H, f, A, b, Aeq, beq);
         std::cout<<"u_qpoases: "<<(u_qpoases).transpose()<<std::endl;
 
-        std::cout<<"error: "<<(u_proxqp-u_qpoases).norm()<<std::endl;
+        error_list.emplace_back((u_proxqp-u_qpoases).norm());
+
+        std::cout<<"error: "<<error_list.at(i)<<std::endl;
         std::cout<<"---------------"<<std::endl;
+
+        i++;
     }
+    VectorXd vec_error_list = Eigen::Map<VectorXd>(error_list.data(), error_list.size());
+    std::cout<<"error list: "<<std::endl;
+    std::cout<<vec_error_list.transpose()<<std::endl;
     return 0;
 }
